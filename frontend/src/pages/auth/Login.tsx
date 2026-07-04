6844b19125dbbf908d542c07f747d3394f8f6328
favil1506 <fabian.vilchez@unmsm.edu.pe>
2026-07-04 16:10:03 -0500

    mostrar contraseña en logueo

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -116,7 +116,7 @@ function Login() {
                   className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
                   tabIndex={-1}
                 >
-                  {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
+                  {showPassword ? <EyeOff className="w-4 h-4" /> : <Eye className="w-4 h-4" />}
                 </button>
             </div>
             </div>
