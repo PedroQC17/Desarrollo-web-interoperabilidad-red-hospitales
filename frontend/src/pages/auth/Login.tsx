e4dbfc67b7f6b242224d5c11f5efbddb75a8141b
favil1506 <fabian.vilchez@unmsm.edu.pe>
2026-07-04 16:09:48 -0500

    agregar boton ojo login

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -110,6 +110,14 @@ function Login() {
                   onKeyDown={handleKeyDown}
                   style={{ paddingRight: "40px", width: "100%" }}
                 />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword((p) => !p)}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
+                  tabIndex={-1}
+                >
+                  {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
+                </button>
             </div>
             </div>
           </div>
